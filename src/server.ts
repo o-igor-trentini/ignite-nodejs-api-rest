dfd81be7e47b0eae20dc6cd0d60e3eef5149e265
Igor Trentini <igortrentini.2004@gmail.com>
2023-12-07 13:30:22 -0300

    feat: Configurado banco de dados e migrations

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello world!'
+app.get('/', async () => {
+  // return await knex('sqlite_schema').select('*')
 })
 
 app.listen({ port: 3000 }).then(() => console.log('Servidor executando!'))
