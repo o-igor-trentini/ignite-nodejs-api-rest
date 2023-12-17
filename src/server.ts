45e1ce409e182daf44ec652358574667b0e6dcee
Igor Trentini <igortrentini.2004@gmail.com>
2023-12-17 14:01:09 -0300

    feat: Tabela de transações com migrations

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/', async () => {
-  // return await knex('sqlite_schema').select('*')
+  return await knex('sqlite_schema').select('*')
 })
 
 app.listen({ port: 3000 }).then(() => console.log('Servidor executando!'))
