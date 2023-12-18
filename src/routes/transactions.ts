bdc614dfbac24694535eebe0c3f03ebc2ede9212
Igor Trentini <igortrentini.2004@gmail.com>
2023-12-18 20:54:29 -0300

    feat: Buscar todas as transações e por ID

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import crypto from 'crypto'
 import { z } from 'zod'
 
 export const transactions = async (app: FastifyInstance) => {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (req: FastifyRequest) => {
+    const schema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = schema.parse(req.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
   app.post('/', async (req: FastifyRequest, res: FastifyReply) => {
     const schema = z.object({
       title: z.string(),
